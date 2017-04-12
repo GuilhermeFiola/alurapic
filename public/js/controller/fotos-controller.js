2424b62c29745e956218d0c2ced70415ccc50d0f
Guilherme Fiola <guilhermefiola@gmail.com>
2017-04-11 22:23:36 -0300

    Aula 03

--- public/js/controller/fotos-controller.js
+++
@@ -1,6 +0,0 @@
-angular.module("Alurapic").controller("FotosController", function($scope) {
-    $scope.foto = {
-        titulo: "Leão",
-        url: "http://www.fundosanimais.com/Minis/leoes.jpg"
-    }
-});
